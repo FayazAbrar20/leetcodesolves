87e41395a310e798a12296cee64d1559d904ff86
FayazAbrar20 <fayazabrar2000@gmail.com>
2026-08-05 22:11:47 +0600

    Time: 278 ms (61.18%), Space: 66.4 MB (98.75%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.py
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -3,5 +3,5 @@ import pandas as pd
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
     value = person["email"][person["email"].duplicated()].drop_duplicates()
 
-    if(len(value) != None):
-        return pd.DataFrame({"Email":value})
+    
+    return pd.DataFrame({"Email":value})
